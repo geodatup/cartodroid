50d901d623c4c7d4f01759fbabc60284b200747b
Hugo Roussaffa <hugoroussaffa@gmail.com>
2016-08-10 22:41:41 +0200

    chmod error

--- script/install_wifi_AP.sh
+++ script/install_wifi_AP.sh
@@ -1,32 +1,27 @@
 #!/bin/sh
 
-sudo apt-get **install** dnsmasq hostapd &&
+mv /etc/network/interfaces.hostapd /etc/network/interfaces.hostapd.bkp &&
 
-wget -P /etc https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/dhcpcd.conf &&
+ln -s /etc/network/interfaces /etc/network/interfaces.hostapd &&
 
-wget -P /etc/network/interfaces.d https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/wlan0 &&
+wget -P /etc/network https://raw.githubusercontent.com/geodatup/geodroid/master/wifi_AP_src/interfaces.hostapd &&
 
-sudo service dhcpcd restart &&
+#sudo ifdown wlan0; sudo ifup wlan0 &&
 
-sudo ifdown wlan0; sudo ifup wlan0 &&
+wget -P /etc/hostapd https://raw.githubusercontent.com/geodatup/geodroid/master/wifi_AP_src/hostapd.conf &&
 
-wget -P /etc/hostapd https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/hostapd.conf &&
 
-#sudo /usr/sbin/hostapd /etc/hostapd/hostapd.conf 
+wget -P /etc/init.d https://raw.githubusercontent.com/geodatup/geodroid/master/wifi_AP_src/hostapd &&
 
-**mv** /etc/default/hostapd /etc/default/hostapd.bak &&
+sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig &&
 
-wget -P /etc/default https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/hostapd &&
+wget -P /etc https://raw.githubusercontent.com/geodatup/geodroid/master/wifi_AP_src/dnsmasq.conf &&
 
-sudo **mv** /etc/dnsmasq.conf /etc/dnsmasq.conf.orig &&
+sudo mv /etc/sysctl.conf /etc/sysctl.conf.bak &&
 
-wget -P /etc https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/dnsmasq.conf &&
+wget -P /etc https://raw.githubusercontent.com/geodatup/geodroid/master/wifi_AP_src/sysctl.conf &&
 
-sudo **mv** /etc/sysctl.conf /etc/sysctl.conf.bak &&
-
-wget -P /etc https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/sysctl.conf &&
-
-sudo sh -c **"echo 1 > /proc/sys/net/ipv4/ip_forward"** &&
+sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward" &&
 
 sleep 5
 
@@ -38,13 +33,13 @@ sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT 
 
 sleep 5
 
-sudo sh -c **"iptables-save > /etc/iptables.ipv4.nat"** &&
+sudo sh -c "iptables-save > /etc/iptables.ipv4.nat" &&
 
-sudo **mv** /etc/rc.local /etc/rc.local.bak &&
+#sudo mv /etc/rc.local /etc/rc.local.bak &&
 
-wget -P /etc https://raw.githubusercontent.com/jancelin/rpi_wifi_direct/master/raspberry_pi3/rc.local &&
+wget -P /etc https://raw.githubusercontent.com/geodatup/geodroid/master/wifi_AP_src/rc.local &&
 
-**chmod** +x  /etc/rc.local &&
+chmod +x rc.local &&
 
 sudo service hostapd start 
 
