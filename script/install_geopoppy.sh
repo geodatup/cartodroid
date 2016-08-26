793745aa19281ff562236be0ed06558d1626b7ca
Hugo Roussaffa <hugoroussaffa@gmail.com>
2016-08-26 11:03:19 +0200

    remove docker file from jancelin archive

--- script/install_geopoppy.sh
+++ script/install_geopoppy.sh
@@ -14,5 +14,8 @@ wget -P /home/geodatup https://github.com/jancelin/geo-poppy/raw/master/install/
 tar xvfz /home/geodatup/geopoppy_sig.tar --preserve-permissions --same-owner -C / &&
 tar xvfz /home/geodatup/geopoppy_base.tar --preserve-permissions --same-owner -C / &&
 
-cd /home/Geopoppy &&
-docker-compose up -d &&
+cd /home/GeoPoppy &&
+rm docker-compose.yml
+wget https://raw.githubusercontent.com/geodatup/cartodroid/master/script/docker-compose.yml
+chmod +x docker-compose.yml 
+docker-compose up -d
