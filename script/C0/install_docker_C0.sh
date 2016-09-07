4622c47a4169fdbbd6e8a91a4649e340643cfbf3
Hugo Roussaffa <hugoroussaffa@gmail.com>
2016-09-07 16:51:20 +0200

    adjust

--- script/C0/install_docker_C0.sh
+++ script/C0/install_docker_C0.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-sudo apt-get update
-sudo apt-get upgrade -y
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
- 
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo "deb https://packagecloud.io/Hypriot/Schatzkiste/debian/ wheezy main" > /etc/apt/sources.list.d/hypriot.list 
 curl https://packagecloud.io/gpg.key | sudo apt-key add - 
 apt-get -y install --no-install-recommends docker-hypriot 
